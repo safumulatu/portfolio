021932ac809b8f347dca0cabfb9d7dc482ac9056
safumulatu <safumulatu9@gmail.com>
2024-04-03 12:03:00 +0300

    oops

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+const App = () => {
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">parallex</section>
+      <section>service</section>
+      <section id="Portfolio">parallex</section>
+      <section>portfolio1</section>
+      <section>portfolio2</section>
+      <section>portfolio3</section>
+      <section id="Contact">contact</section>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hero/Hero.jsx
@@ -0,0 +1,5 @@
+import "./hero.scss";
+function Hero() {
+  return <div>Hero</div>;
+}
+export default Hero;
